495198d5cb58cf713ed23467ba09a40c6476be7a
Kamen Yanakiev <Kamen_Yanakiev@epam.com>
2021-03-24 15:41:42 +0200

    footer done, added font awesome icons and more common styles

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public categories = [
+    'Graphics', 'Addons', 'Buttons', 'Infographics',
+    'PSDs', 'Fonts', 'Forms', 'Graphs',
+    'Icons', 'Textures', 'GUI', 'Logos',
+    'Templates', 'Patterns', 'Navigation', 'Vectors',
+    'Themes', 'Backgrounds', 'UI Kits', 'Business Cards'
+  ];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
